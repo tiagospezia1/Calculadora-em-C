84b62d43991976b54286a9a0738b9b8206aa4e2e
Tiago Spezia <78884428+tiagospezia1@users.noreply.github.com>
2025-03-24 14:33:12 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,99 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    int operacao;
+    float n1,n2,r;
+    
+    printf("1=+\n");
+    printf("2=-\n");
+    printf("3=*\n");
+    printf("4=/\n");
+    printf("Digite o Tipo de operacao:\n");
+    scanf("%i",&operacao);
+    fflush(stdin);
+    
+    printf("Digite o Primeiro Numero\n");
+    scanf("%f",&n1);
+    fflush(stdin);
+    
+    printf("Digite o Segundo Numero\n");
+    scanf("%f",&n2);
+    fflush(stdin);
+    
+
+    
+
+    
+if (operacao==1){
+
+    
+
+r = n1 + n2;  
+
+    printf("%f",r);
+
+
+    
+}
+
+else{
+    
+    }
+    
+    if (operacao==2){
+
+    
+
+r = n1 - n2;  
+
+    printf("%f",r);
+
+
+    
+}
+
+else{
+    
+    }
+    
+        if (operacao==3){
+
+    
+
+r = n1 * n2;  
+
+    printf("%f",r);
+
+
+    
+}
+
+else{
+    
+    }
+    
+        if (operacao==4){
+
+    
+
+r = n1 / n2;  
+
+    printf("%f",r);
+
+}
+
+else{
+    
+    }
+    
+    return 0;
+}
